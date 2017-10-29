1626089cd3e166857b89d149d614305fdf9690b6
tvanh512 <anhtv1@gmail.com>
2017-10-29 14:47:43 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,4 +1,5 @@
 <?php
+<header class="navbar navbar-default navbar-static-top">
 <div class="w3-top">
   <div class="w3-bar w3-theme w3-top w3-left-align w3-large">
     <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large w3-theme-l1" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
@@ -9,4 +10,5 @@
     <a href="blog.html" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Blog</a>
   </div>
 </div>
+</header>
 ?>
